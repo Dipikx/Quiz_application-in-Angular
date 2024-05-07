9abe2d32bcbc186ddc1d3e5a2ae37924354dbe59
Dipikx <138647019+Dipikx@users.noreply.github.com>
2024-05-07 19:16:24 +0530

    Add files via upload

---
+++ welcome.component.ts
@@ -0,0 +1,18 @@
+import { Component , OnInit,ViewChild,ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome',
+  templateUrl: './welcome.component.html',
+  styleUrl: './welcome.component.css'
+})
+export class WelcomeComponent implements OnInit {
+
+  @ViewChild('name') nameKey! : ElementRef;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  startQuiz(){
+    localStorage.setItem("name",this.nameKey.nativeElement.value);
+  }
+}
